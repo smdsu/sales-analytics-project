811b654f71fd141829b81e799e791a5ce49d1117
smdsu <109036946+smdsu@users.noreply.github.com>
2024-12-30 19:20:52 +0300

    Passlib prep
    Preparing passlib

--- app/config.py
+++ app/config.py
@@ -7,6 +7,9 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    SECRET_KEY: str
+    ALGORITHM: str
+
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
     )
@@ -15,3 +18,6 @@ settings = Settings()
 
 def get_db_url():
     return f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+def get_auth_data():
+    return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
