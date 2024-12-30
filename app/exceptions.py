b5eba0cad2129463d87ac373f878010ee47f6cfb
smdsu <109036946+smdsu@users.noreply.github.com>
2024-12-30 19:22:58 +0300

    Add exceptions handler

---
+++ app/exceptions.py
@@ -0,0 +1,21 @@
+from fastapi import status, HTTPException
+
+class TokenExpiredException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+
+class TokenNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token not found")
+
+class NoJwtException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="No JWT")
+
+class NoUserIdException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="No user ID")
+    
+class NoUserException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.logger import logger
+
+from app.exceptions import TokenExpiredException, TokenNotFoundException, NoUserIdException, NoUserException, NoJwtException
+
 from app.customers.router import router as router_customers
 from app.products.router import router as router_products
 from app.saledetails.router import router as router_saledetails
 from app.sales.router import router as router_sales
+from app.users.router import router as router_users
 
 app = FastAPI()
 
@@ -11,8 +17,33 @@ app = FastAPI()
 def home_page():
     return {"message": "Привет мир API!"}
 
-
 app.include_router(router_customers)
 app.include_router(router_products)
 app.include_router(router_sales)
 app.include_router(router_saledetails)
+app.include_router(router_users)
+
+@app.exception_handler(TokenExpiredException)
+async def token_expired_exception_handler(request: Request, exc: TokenExpiredException):
+    logger.error(exc)
+    return RedirectResponse(url="/auth/")
+
+@app.exception_handler(TokenNotFoundException)
+async def token_not_found_exception_handler(request: Request, exc: TokenNotFoundException):
+    logger.error(exc)
+    return RedirectResponse(url="/auth/")
+
+@app.exception_handler(NoUserIdException)
+async def no_user_id_exception_handler(request: Request, exc: NoUserIdException):
+    logger.error(exc)
+    return RedirectResponse(url="/auth/")
+
+@app.exception_handler(NoUserException)
+async def no_user_exception_handler(request: Request, exc: NoUserException):
+    logger.error(exc)
+    return RedirectResponse(url="/auth/")
+
+@app.exception_handler(NoJwtException)
+async def no_jwt_exception_handler(request: Request, exc: NoJwtException):
+    logger.error(exc)
+    return RedirectResponse(url="/auth/")
